03033d0d755c7f715186683d236fcb42f343c8a5
Huda Aeda <hudaaeda10@gmail.com>
2021-03-28 16:29:47 +0700

    Pertemuan #25 - Many To Many Relationship

--- project-pertama/app/User.php
+++ project-pertama/app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function role()
+    {
+        return $this->belongsToMany('App\Models\Role', 'akses_user', 'user_id', 'master_akses_id');
+    }
 }
