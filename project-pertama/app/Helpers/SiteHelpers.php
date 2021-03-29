701f3701bbf2b1b47bf21b38653a5748ce8c4f55
Huda Aeda <hudaaeda10@gmail.com>
2021-03-29 19:52:59 +0700

    Pertemuan #31 - Manajemen Akses - Navigasi Menu Part 2

--- project-pertama/app/Helpers/SiteHelpers.php
+++ project-pertama/app/Helpers/SiteHelpers.php
@@ -2,10 +2,30 @@
 
 namespace App\Helpers;
 
+use Illuminate\Support\Facades\DB;
+
 class SiteHelpers
 {
     public static function cek_akses()
     {
         return 'mulai dari null';
+    }
+
+    public static function main_menu()
+    {
+        $main_menu = DB::table('akses')->join('menu', 'menu.id', '=', 'akses.menu_id')
+            ->select('menu.*', 'akses.akses', 'akses.tambah', 'akses.edit', 'akses.hapus')
+            ->where('akses.level_user_id', '1')
+            ->where('menu.level_menu', 'main_menu')->get();
+        return $main_menu;
+    }
+
+    public static function sub_menu()
+    {
+        $sub_menu = DB::table('akses')->join('menu', 'menu.id', '=', 'akses.menu_id')
+            ->select('menu.*', 'akses.akses', 'akses.tambah', 'akses.edit', 'akses.hapus')
+            ->where('akses.level_user_id', '1')
+            ->where('menu.level_menu', 'sub_menu')->get();
+        return $sub_menu;
     }
 }
--- project-pertama/resources/views/layouts/sidebar.blade.php
+++ project-pertama/resources/views/layouts/sidebar.blade.php
@@ -11,14 +11,14 @@
             <li class="@if (Request::segment(1) == 'dashboard') active @endif""><a class="nav-link" href="{{ url('dashboard') }}"><i class="far fa-square"></i>
                 <span>Dashboard</span></a></li>
             <li class="menu-header">Menu</li>
-            <li class="nav-item dropdown @if (Request::segment(1) == 'konfigurasi') active @endif">
+            {{-- <li class="nav-item dropdown @if (Request::segment(1) == 'konfigurasi') active @endif">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
                     <span>Konfigurasi Aplikasi</span></a>
                 <ul class="dropdown-menu">
                     <li class="@if (Request::segment(1) == 'konfigurasi' and Request::segment(2) == 'setup') active @endif"><a class="nav-link" href="{{ route('setup.index') }}">Setup Aplikasi</a></li>
                 </ul>
-            </li>
-            <li class="nav-item dropdown @if (Request::segment(1) == 'master-data') active @endif">
+            </li> --}}
+            {{-- <li class="nav-item dropdown @if (Request::segment(1) == 'master-data') active @endif">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
                     <span>Masterdata</span></a>
                     <ul class="dropdown-menu">
@@ -28,7 +28,21 @@
                     <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>
                     <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
                 </ul>
+            </li> --}}
+            @foreach (SiteHelpers::main_menu() as $mm)
+            <li class="nav-item dropdown @if (Request::segment(1) == 'master-data') active @endif">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
+                    <span>{{$mm->nama_menu}}</span></a>
+                    <ul class="dropdown-menu">
+                        @foreach (SiteHelpers::sub_menu() as $sm)
+                        @if ($sm->master_menu == $mm->id)
+                            <li><a class="nav-link" href="{{ url($sm->url) }}">{{$sm->nama_menu}}</a></li>
+                        @endif
+                        @endforeach
+                </ul>
             </li>
+            @endforeach
+
             <li class="@if (Request::segment(1) == 'crud' and Request::segment(2) == 'index') active @endif"><a class="nav-link" href="{{ route('crud') }}"><i class="far fa-square"></i>
                     <span>CRUD</span></a></li>
             {{-- <li class="nav-item dropdown">
